a8fb74746a91869580a26533b4cb0b828a08f871
kashif <syedraz1212@gmail.com>
2024-05-31 11:40:34 +0500

    gradient bg admin dashboard blocks

--- resources/views/dashboards/admin.blade.php
+++ resources/views/dashboards/admin.blade.php
@@ -35,18 +35,14 @@ $data = json_encode($entries);
                     <h2 class="text-lg font-medium text-gray-900">
                         {{ __('Members Statistics') }} <span class="bg-green-100 text-green-800 ml-2 text-sm font-medium me-2 px-2.5 py-0.5 rounded">{{$total_users}} registered members</span>
                     </h2>
-                    <div class="grid grid-cols-1 gap-4 px-4 mt-8 lg:grid-cols-4 sm:px-8">
+                    <div class="grid grid-cols-1 gap-4 px-4 mt-8 lg:grid-cols-5 sm:px-8">
                         @foreach ($affiliations as $affiliation)
                         @php
                         $user_count = User::where("affiliations","=",$affiliation->id)->count();
                         @endphp
-                        <div class="flex items-center bg-white border rounded-sm overflow-hidden shadow">
-                            <div class="p-4 bg-blue-400 h-20 w-20 text-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 mt-2 ml-2 text-white" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
-                                    <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
-                                </svg>
-                            </div>
-                            <div class="px-4 text-gray-700">
+                        <div class="bg-gradient-to-r from-cyan-500 from-5% via-blue-500 via-40% to-indigo-500 to-90% flex items-center  justify-center bg-white rounded-lg overflow-hidden shadow-md">
+                           
+                            <div class="p-3 text-gray-50 text-center">
                                 <h3 class="text-md tracking-wider">{{$affiliation->affiliation_title}} <button data-popover-target="popover-{{$affiliation->id}}" data-popover-placement="bottom" type="button"><span class="sr-only">Show information</span><i class="fa-solid fa-circle-question"></i></button></h3>
                                 <p class="text-2xl">{{$user_count == 1? $user_count." member" :$user_count." members"  }} </p>
                                 <div data-popover id="popover-{{$affiliation->id}}" role="tooltip" class="absolute z-10 invisible inline-block text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-72 ">

b8487897607fe954a2b11bd6c2d11803805681b2
kashif <syedraz1212@gmail.com>
2024-06-11 15:43:58 +0500

    user count error sorted

--- resources/views/dashboards/admin.blade.php
+++ resources/views/dashboards/admin.blade.php
@@ -73,7 +73,7 @@ $data = json_encode($entries);
               <div class="p-2 text-gray-700 text-center">
                 <h3 class="text-md font-bold tracking-wider hover:underline mb-1"><a href="{{route('affiliation.show', $affiliation->id)}}"> {{$affiliation->affiliation_title}}</a> <button data-popover-target="popover-{{$affiliation->id}}" data-popover-placement="bottom" type="button"><span class="sr-only">Show information</span><i class="fa-solid fa-circle-question"></i></button></h3>
                 <div class="bg-white  rounded-lg text-gray-700">
-                  <p class=" p-2 "> <b>Total Profiles: </b> {{1000 }}  </p>
+                  <p class=" p-2 "> <b>Total Profiles: </b> {{$user_count }}  </p>
                 </div>
 
                 <div class="grid grid-cols-3 gap-3 mt-2">
